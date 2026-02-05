a3434690cf96114a3e1bf289c8dc7486125afd42
sandftae <sandftae@mail.com>
2026-02-05 19:36:36 +0200

    added updates to color schemas

--- source/bash/app/core/uiAdapter.sh
+++ source/bash/app/core/uiAdapter.sh
@@ -7,26 +7,42 @@
 # asks user to select preferred UI mode
 uiSelectMode() {
     local choice
+    local header_text
+    header_text="How would you like to do the $(printf "%bstep-by-step setup?%b" "${ANSI_COLOR_BOLD}" "${ANSI_COLOR_NORMAL}")"
 
     # clear just a little bit
     clear
 
     # hero section
     gum style \
-        --foreground "$COLOR_PRIMARY" \
-        --border double \
-        --border-foreground "$COLOR_ACCENT" \
-        --align center \
-        --width 82 \
-        --margin "1 2" \
-        --padding "0 1" \
-        "$APP_LOGO"
+          --foreground "$COLOR_PRIMARY" \
+          --border double \
+          --border-foreground "$COLOR_ACCENT" \
+          --align center --width 60 --margin "1 2" --padding "0 1" \
+          "$APP_TITLE"
 
     # just visual effects, nothing more
-    gum spin --spinner "$UI_SPINNER_TYPE" --title " Preparing setup modules..." -- sleep 0.6
+    gum spin \
+        --spinner.margin "0 0 0 3" \
+        --spinner.padding "0 0 0 0" \
+        --spinner.foreground "$COLOR_SPINNER_ACCENT" \
+        --title " Preparing builder ..." \
+        -- sleep 0.7
 
-    # interaction
-    choice=$(gum choose --header "Select your configuration style:" "$LABEL_MODERN" "$LABEL_CLASSIC")
+    # interaction with proper indentation
+    choice=$(gum choose \
+        --header "$header_text" \
+        --header.margin "0 0 0 3" \
+        --item.margin "0 0 0 3" \
+        --item.padding "0 0 0 0" \
+        --selected.margin "0 0 0 3" \
+        --cursor.padding="0 3 0 0" \
+        --cursor="     >" \
+        --selected-prefix="• " \
+        --unselected-prefix="  " \
+        --selected.foreground="$COLOR_ACCENT" \
+        "$LABEL_MODERN" "$LABEL_CLASSIC")
+
     local exitCode=$?
 
     [[ $exitCode -eq 130 || -z "$choice" ]] && {
